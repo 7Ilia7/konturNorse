b9995afbb54c38b382636896d266ecdaeb3fb00c
7iilia7 <greench707@gmail.com>
2023-01-04 23:55:14 +0200

    add description of all methods

--- base_object/base.py
+++ base_object/base.py
@@ -10,29 +10,63 @@ class BaseObject:
 
     def is_visible(self, locator):
         """
-
-        :param locator:
-        :return:
+        to find visible element
+        :param locator: is responsible for element
+        :return: visible element
         """
         return self.wait.until(ec.visibility_of_element_located(locator))
 
+
     def is_click(self, locator):
+        """
+        element clickability indicator
+        :param locator: is responsible for element
+        :return: clickable state of an element
+        """
         return self.wait.until(ec.element_to_be_clickable(locator))
 
     def get_text(self, locator):
+        """
+        get text from element
+        :param locator: is responsible for element
+        :return: text from element
+        """
         return self.is_visible(locator).text
 
     @staticmethod
     def assertion(actual, expected):
+        """
+        Comparison of actual and expected result
+        :param actual: the result that is now
+        :param expected: the result we want to get
+        :return: compliance or non-compliance of the actual result with the expected
+        """
         assert actual == expected, f'Failed. Expected {expected}, but got {actual}'
 
     def to_click(self, locator):
+        """
+        to click on visible element
+        :param locator: is responsible for element
+        :return: Clicking on an element
+        """
         self.is_click(locator).click()
 
     def to_send_keys(self, locator, data):
+        """
+        Send values like "greench" or "123"
+        :param locator: is responsible for element
+        :param data: values
+        :return: fills the input field with data
+        """
         self.is_visible(locator).send_keys(data)
 
     def is_disappeared(self, locator, locator2):
+        """
+
+        :param locator:
+        :param locator2:
+        :return:
+        """
         try:
             self.is_visible(locator)
         except NoSuchElementException:
