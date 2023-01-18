df2a94a6272a36abd10fa94f6b2de6ff0a061c48
7iilia7 <greench707@gmail.com>
2023-01-18 19:24:17 +0200

    triger

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,9 +1,10 @@
 import time
 
 
-def test_login_check_check_data_package_menu(index_page, main_page):
+def test_login_check_data_package_menu_button(index_page, main_page):
     index_page.login_flow()
     main_page.click_on_logo()
     main_page.click_on_data_package_menu()
     main_page.check_in_data_package_menu()
 
+#re
